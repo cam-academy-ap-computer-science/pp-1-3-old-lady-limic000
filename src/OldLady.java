f0ab9fa6e8ffada3c1b97bf7c9aa73402255c089
Michelle Li <limichelle009@gmail.com>
2019-09-15 21:44:56 -0700

    Finished at home.

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,8 +2,94 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		v1();
+		System.out.println();
+		v2();
+		System.out.println();
+		v3();
+		System.out.println();
+		v4();
+		System.out.println();
+		v5();
+		System.out.println();
+		v6();
 
 	}
+	
+	public static void idk(){
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+	}
+	
+	public static void spider() {
+		System.out.println("She swallowed the spider to catch the fly,");
+	}
+	
+	public static void bird() {
+		System.out.println("She swallowed the bird to catch the spider,");
+	}
+	
+	public static void cat() {
+		System.out.println("She swalled the cat to catch the bird,");
+	}
 
+	public static void dog() {
+		System.out.println("She swallowed the dog to catch the cat,");
+	}
+	
+	public static void a_lady() {
+		System.out.print("There was an old lady who swallowed ");
+	}
+	
+	public static void v1() {
+		a_lady();
+		System.out.println("who swallowed a fly.");
+		idk();
+	}
+	
+	public static void v2() {
+		a_lady();
+		System.out.println("swallowed a spider,");
+		System.out.println("That wiggled and iggled and jiggled inside her.");
+		spider();
+		idk();
+	}
+	
+	public static void v3() {
+		a_lady();
+		System.out.println("a bird,");
+		System.out.println("How absurd to swallow a bird.");
+		bird();
+		spider();
+		idk();
+	}
+	
+	public static void v4() {
+		a_lady();
+		System.out.println("a cat,");
+		System.out.println("Imagine that to swallow a cat.");
+		cat();
+		bird();
+		spider();
+		idk();
+	}
+	
+	public static void v5() {
+		a_lady();
+		System.out.println("a dog,");
+		System.out.println("What a hog to swallow a dog.");
+		dog();
+		cat();
+		bird();
+		spider();
+		idk();
+	}
+	
+	public static void v6() {
+		a_lady();
+		System.out.println("a horse,");
+		System.out.println("She died of course.");
+	}
+	
+	
 }
